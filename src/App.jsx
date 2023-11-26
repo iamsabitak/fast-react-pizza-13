b5a28843cf5c14a5c2f4237fcafee033b2767878
sabita <sabitakhadka091@gmail.com>
2023-11-26 14:38:45 +0545

    Configured React Router routes with error handling and loaders

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,26 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/:orderId", element: <Order /> },
+      { path: "/order/:orderId", element: <Order />, loader: orderLoader },
       { path: "/order/new", element: <CreateOrder /> },
     ],
   },
