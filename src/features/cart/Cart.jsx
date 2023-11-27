5d215888adeff8d3c542f9ef97c5e1324529268a
sabita <sabitakhadka091@gmail.com>
2023-11-27 19:10:38 +0545

    Developed Cart component for displaying items and ordering functionality.

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -34,7 +34,7 @@ function Cart() {
     <div className="px-4 py-3">
       <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+       <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
 
       <ul className="mt-3 divide-y divide-stone-200 border-b">
         {cart.map((item) => (
