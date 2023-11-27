5c5cbe1db789c61e27f967963deb95737c952e75
sabita <sabitakhadka091@gmail.com>
2023-11-27 19:08:25 +0545

    Implemented Menu component to display pizza items from the restaurant menu.

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -8,7 +8,7 @@ function Menu() {
   return (
     <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
-        <MenuItem pizza={pizza} key={pizza.id} />
+         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
   );
