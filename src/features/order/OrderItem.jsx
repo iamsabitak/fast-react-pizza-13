b14b0042c19453003877692cd9de3ceb6123cd6b
sabita <sabitakhadka091@gmail.com>
2023-11-27 19:05:10 +0545

    Implemented OrderItem component to display item details and prices.

--- src/features/order/OrderItem.jsx
+++ src/features/order/OrderItem.jsx
@@ -7,7 +7,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
     <li className="py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
         <p>
-          <span className="font-bold">{quantity}&times;</span> {name}
+           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
       </div>
