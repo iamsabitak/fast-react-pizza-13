08755766093068e21a5fe482b47d11a2181148ea
sabita <sabitakhadka091@gmail.com>
2023-11-27 18:48:25 +0545

    Adjust error handling and navigation in Error component

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -7,8 +7,8 @@ function Error() {
 
   return (
     <div>
-      <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
+     <h1>Something went wrong 😢</h1>
+       <p>{error.data || error.message}</p>
 
       <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
