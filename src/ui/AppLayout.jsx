a11df35264464332060d0e8497807b8604151b14
sabita <sabitakhadka091@gmail.com>
2023-11-27 18:49:17 +0545

    fix: Correct grid layout class name and structure in AppLayout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -11,7 +11,7 @@ function AppLayout() {
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
-      <Header />
+       <Header />
 
       <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
