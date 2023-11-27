418168af2eb3c27194ea0f452108e694288aeae0
sabita <sabitakhadka091@gmail.com>
2023-11-27 18:46:40 +0545

    style: Refine styling and Add CreateUser component to Home

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -6,9 +6,9 @@ function Home() {
       <h1 className="mb-8  text-xl font-semibold md:text-3xl">
         The best pizza.
         <br />
-        <span className="text-yellow-500">
+       <span className="text-yellow-500">
           Straight out of the oven, straight to you.
-        </span>
+       </span>
       </h1>
 
       <CreateUser />
