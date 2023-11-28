fb41b96d91cdbb8444d06336f9204af289266455
sabita <sabitakhadka091@gmail.com>
2023-11-28 15:38:38 +0545

    Implement Loader component for loading state indication

--- src/ui/loader.jsx
+++ src/ui/loader.jsx
@@ -1,7 +1,7 @@
 function Loader() {
   return (
     <div className="absolute inset-0 flex items-center justify-center bg-slate-200/20 backdrop-blur-sm">
-       <div className="loader"></div>
+      <div className="loader"></div>
     </div>
   );
 }
